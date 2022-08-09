f5bab2ed9b92ba564a5c7f3425f9b4d17f6e7be8
creator54 <hi.creator54@gmail.com>
2022-08-09 12:53:53 +0530

    add compiled data

---
+++ _site/default.nix
@@ -0,0 +1,12 @@
+with (import <nixpkgs> {});
+let
+  gems = bundlerEnv {
+    name = "blog";
+    inherit ruby;
+    gemdir = ./.;
+  };
+in stdenv.mkDerivation {
+  name = "blog";
+  buildInputs = [gems ruby bundix];
+}
+
